e6ba3b80047550a46e0904be27464886562c210d
Teenapat <teenapat.wu@gmail.com>
2020-10-27 10:16:58 +0700

    config fire base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,28 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SignupComponent } from './components/signup/signup.component';
 
-// Routes
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
 export const router: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'profile', component: ProfileComponent }
 ]
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCRpAiTvNcZoWhHMvD0i4Pz8qwx-579Azs",
+  authDomain: "angular-test-df9ae.firebaseapp.com",
+  databaseURL: "https://angular-test-df9ae.firebaseio.com",
+  projectId: "angular-test-df9ae",
+  storageBucket: "angular-test-df9ae.appspot.com",
+  messagingSenderId: "840450488804",
+  appId: "1:840450488804:web:5397cded205e35fcaaaff1",
+  measurementId: "G-CFJH5RJMXK"
+};
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,7 +44,9 @@ export const router: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(router)
+    RouterModule.forRoot(router),
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
