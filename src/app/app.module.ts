6f72e192378aa50b64edbca8fffd633f0411c609
Teenapat <teenapat.wu@gmail.com>
2020-10-29 13:58:14 +0700

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,14 +26,14 @@ export const router: Routes = [
 ]
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCRpAiTvNcZoWhHMvD0i4Pz8qwx-579Azs",
-  authDomain: "angular-test-df9ae.firebaseapp.com",
-  databaseURL: "https://angular-test-df9ae.firebaseio.com",
-  projectId: "angular-test-df9ae",
-  storageBucket: "angular-test-df9ae.appspot.com",
-  messagingSenderId: "840450488804",
-  appId: "1:840450488804:web:5397cded205e35fcaaaff1",
-  measurementId: "G-CFJH5RJMXK"
+  apiKey: 'AIzaSyCRpAiTvNcZoWhHMvD0i4Pz8qwx-579Azs',
+  authDomain: 'angular-test-df9ae.firebaseapp.com',
+  databaseURL: 'https://angular-test-df9ae.firebaseio.com',
+  projectId: 'angular-test-df9ae',
+  storageBucket: 'angular-test-df9ae.appspot.com',
+  messagingSenderId: '840450488804',
+  appId: '1:840450488804:web:5397cded205e35fcaaaff1',
+  measurementId: 'G-CFJH5RJMXK'
 };
 
 @NgModule({
